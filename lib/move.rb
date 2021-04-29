12f71014b63b89c61353ef21aa042d8d6ec48bef
Thomas Eastman <the12394@gmail.com>
2021-04-29 01:21:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,3 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -14,7 +13,7 @@ def input_to_index(index = gets.strips)
   index.to_i - 1
 end
 
-def move(board, index, value = "X")
+def move(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "], index, value = "X")
   board[index] = value
 end
 
